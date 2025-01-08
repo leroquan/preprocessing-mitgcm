7e83f54220c5edaec07b710e01360ed91fb94f93
leroquan <anne.leroquais@eawag.ch>
2025-01-08 11:40:47 +0100

    Simplified grid creation

--- grid_and_bathy/grid_functions.py
+++ grid_and_bathy/grid_functions.py
@@ -5,26 +5,29 @@ import pandas as pd
 from pyproj import Transformer
 
 
-def create_regular_grid(Nx: int, Ny: int, grid_resolution: int) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+class MitgcmGrid:
+    def __init__(self, path_grid):
+        self.x = np.load(os.path.join(path_grid, 'x.npy'))
+        self.y = np.load(os.path.join(path_grid, 'y.npy'))
+        self.lat_grid = np.load(os.path.join(path_grid, 'lat_grid.npy'))
+        self.lon_grid = np.load(os.path.join(path_grid, 'lon_grid.npy'))
+
+
+def get_grid(path_folder_grid: str) -> MitgcmGrid:
+    return MitgcmGrid(path_folder_grid)
+
+
+def create_regular_grid(nx: int, ny: int, grid_resolution: int) \
+        -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     # XY on mitgcm grid
-    x = np.arange(0, Nx * grid_resolution, grid_resolution)
-    y = np.arange(0, Ny * grid_resolution, grid_resolution)
+    x = np.arange(0, nx * grid_resolution, grid_resolution)
+    y = np.arange(0, ny * grid_resolution, grid_resolution)
     xgrid, ygrid = (np.meshgrid(x, y))
 
     return x, y, xgrid, ygrid
 
 
-def place_grid_on_swiss_grid(x: np.array(float), y: np.array(float), XO_SG: float, YO_SG: float) \
-        -> (np.ndarray, np.ndarray):
-    # XY on swiss grid
-    x_sg = x + XO_SG
-    y_sg = y + YO_SG
-    xsg_grid, ysg_grid = np.meshgrid(x_sg, y_sg)
-
-    return xsg_grid, ysg_grid
-
-
-def translate_grid_to_origin(x: np.ndarray, y: np.ndarray, dx: float, dy: float) -> (np.ndarray, np.ndarray):
+def translate_grid(x: np.ndarray, y: np.ndarray, dx: float, dy: float) -> (np.ndarray, np.ndarray):
     trans_x = x + dx
     trans_y = y + dy
 
@@ -38,12 +41,12 @@ def get_grid_angle(x0: float, y0: float, x1: float, y1: float) -> float:
 
 
 def rotate_grid(x: np.ndarray, y: np.ndarray, x0: float, y0: float, rotation_angle: float) -> (np.ndarray, np.ndarray):
-    x_translated_to_zero, y_translated_to_zero = translate_grid_to_origin(x, y, -x0, -y0)
+    x_translated_to_zero, y_translated_to_zero = translate_grid(x, y, -x0, -y0)
 
     x_rotated = np.cos(rotation_angle) * x_translated_to_zero - np.sin(rotation_angle) * y_translated_to_zero
     y_rotated = np.sin(rotation_angle) * x_translated_to_zero + np.cos(rotation_angle) * y_translated_to_zero
 
-    x_translated_back, y_translated_back = translate_grid_to_origin(x_rotated, y_rotated, x0, y0)
+    x_translated_back, y_translated_back = translate_grid(x_rotated, y_rotated, x0, y0)
 
     return x_translated_back, y_translated_back
 
@@ -68,17 +71,24 @@ def save_grid(folder_path_out: str, x: np.array(float), y: np.array(float), x_ro
         np.save(f, lon_grid)
 
 
-def build_and_save_mitgcm_grid(folder_path_out: str, Nx: int, Ny: int, grid_resolution: int, XO_SG: float, YO_SG: float,
-                               X1_SG: float, Y1_SG: float):
-    x, y, xgrid, ygrid = create_regular_grid(Nx, Ny, grid_resolution)
-    xsg_grid, ysg_grid = place_grid_on_swiss_grid(x, y, XO_SG, YO_SG)
+def build_grid(nx: int, ny: int, grid_resolution: int, x0_sg: float, y0_sg: float, x1_sg: float, y1_sg: float) \
+        -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    x, y, xgrid, ygrid = create_regular_grid(nx, ny, grid_resolution)
+    xsg, ysg = translate_grid(x, y, x0_sg, y0_sg)
+    xsg_grid, ysg_grid = np.meshgrid(xsg, ysg)
 
-    rotation_angle = get_grid_angle(XO_SG, YO_SG, X1_SG, Y1_SG)
-    x_rotated, y_rotated = rotate_grid(xsg_grid, ysg_grid, XO_SG, YO_SG, rotation_angle)
+    rotation_angle = get_grid_angle(x0_sg, y0_sg, x1_sg, y1_sg)
+    x_rotated, y_rotated = rotate_grid(xsg_grid, ysg_grid, x0_sg, y0_sg, rotation_angle)
 
     coord_converter = Transformer.from_crs("EPSG:21781", "EPSG:4326", always_xy=True)
     lon_grid, lat_grid = coord_converter.transform(x_rotated, y_rotated)
 
+    return x, y, x_rotated, y_rotated, lat_grid, lon_grid
+
+
+def build_and_save_mitgcm_grid(folder_path_out: str, nx: int, ny: int, grid_resolution: int, x0_sg: float, y0_sg: float,
+                               x1_sg: float, y1_sg: float):
+    x, y, x_rotated, y_rotated, lat_grid, lon_grid = build_grid(nx, ny, grid_resolution, x0_sg, y0_sg, x1_sg, y1_sg)
     save_grid(folder_path_out, x, y, x_rotated, y_rotated, lat_grid, lon_grid)
 
 
