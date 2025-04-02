97307045de8e28f03c79493b655bba4e97e61b84
leroquan <anne.leroquais@eawag.ch>
2025-04-02 14:52:57 +0200

    Improved syntax in the python script extracting lexplore results

--- 00-template_mitgcm/geneva_base/postprocessing/extract_lexplore_results.py
+++ 00-template_mitgcm/geneva_base/postprocessing/extract_lexplore_results.py
@@ -5,10 +5,13 @@ import os
 
 ds = xm.open_mdsdataset("./", ref_date="2024-03-01 0:0:0", prefix='3Dsnaps', delta_t=32, endian=">")
 
-i_xc = np.argmin(np.abs(ds['XC'].values - 49850))
-i_yc = np.argmin(np.abs(ds['YC'].values - 18100))
-i_xg = np.argmin(np.abs(ds['XG'].values - 49850))
-i_yg = np.argmin(np.abs(ds['YG'].values - 18100))
+x_lexplore = 49850
+y_lexplore = 18100
+
+i_xc = np.argmin(np.abs(ds['XC'].values - x_lexplore))
+i_yc = np.argmin(np.abs(ds['YC'].values - y_lexplore))
+i_xg = np.argmin(np.abs(ds['XG'].values - x_lexplore))
+i_yg = np.argmin(np.abs(ds['YG'].values - y_lexplore))
 
 ds_crop = ds[['THETA','UVEL','VVEL']].isel(XC=i_xc, YC=i_yc, XG=i_xg, YG=i_yg)
 
