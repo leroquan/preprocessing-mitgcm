8e43e61a09a6847887e97ae96deecd6f81ec0a0c
leroquan <anne.leroquais@eawag.ch>
2025-06-06 11:20:18 +0200

    Added doc on function get_grid_angle

--- grid_and_bathy/grid_functions.py
+++ grid_and_bathy/grid_functions.py
@@ -62,6 +62,8 @@ def translate_grid(x: np.ndarray, y: np.ndarray, dx: float, dy: float) -> (np.nd
 
 
 def get_grid_angle(x0: float, y0: float, x1: float, y1: float) -> float:
+    """Compute the angle (in radians) between the positive x-axis and the
+    line segment from point (x0, y0) to point (x1, y1)."""
     grid_angle = np.arctan2(y1 - y0, x1 - x0)
 
     return grid_angle
